971d6fbd1921f8da561c6b817cda9185f3e90bb6
pdey <Pankaj.Dey@viterahealthcare.com>
2013-11-14 13:14:21 +0530

    Creation of Service and consuming it

---
+++ WCFservice/PaperMedia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFservice
+{
+    public class PaperMedia : IMedia
+    {
+        public string GetMediaName()
+        {
+            return "This is test name";
+        }
+    }
+}
---
+++ WCfConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCfConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaService.MediaClient clnt = new MediaService.MediaClient();
+            Console.Write(clnt.GetMediaName());
+            Console.ReadKey();
+        }
+    }
+}
